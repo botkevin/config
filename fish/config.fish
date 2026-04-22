df32c9be2304d625ef6f5fe6507f810d3d070d42
Kevin Shi <shi.kevin.98@gmail.com>
2026-04-21 17:14:26 -0700

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,47 @@
+# fish is in homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    if not set -q TMUX
+        tmux
+    end
 end
 
-  abbr g 'git'
-  abbr ga 'git add'
-  abbr gb 'git branch'
-  abbr gbl 'git blame'
-  abbr gc 'git commit -m'
-  abbr gca 'git commit --amend -m'
-  abbr gco 'git checkout'
-  abbr gcp 'git cherry-pick'
-  abbr gd 'git diff'
-  abbr gf 'git fetch'
-  abbr gl 'git log'
-  abbr gm 'git merge'
-  abbr gp 'git push'
-  abbr gpf 'git push --force-with-lease'
-  abbr gpl 'git pull'
-  abbr gr 'git remote'
-  abbr grb 'git rebase'
-  abbr gs 'git status'
-  abbr gst 'git stash'
+# git abbrs
+abbr g 'git'
+abbr ga 'git add'
+abbr gb 'git branch'
+abbr gbl 'git blame'
+abbr gc 'git commit -m'
+abbr gca 'git commit --amend -m'
+abbr gco 'git checkout'
+abbr gcp 'git cherry-pick'
+abbr gd 'git diff'
+abbr gf 'git fetch'
+abbr gl 'git log'
+abbr gm 'git merge'
+abbr gp 'git push'
+abbr gpf 'git push --force-with-lease'
+abbr gpl 'git pull'
+abbr gr 'git remote'
+abbr grb 'git rebase'
+abbr gs 'git status'
+abbr gst 'git stash'
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# zoxide
+zoxide init fish --cmd cd | source
+
+# pipx
+set PATH $PATH ~/.local/bin
+
+function load_env_vars -d "Load variables in a .env file"
+    set lines (cat $argv | string split -n '\n' | string match -vre '^#')
+    for line in $lines
+        set arr (string split -n -m 1 = $line)
+        if test (count $arr) -ne 2
+            continue
+        end
+        set -gx $arr[1] $arr[2]
+    end
+end
